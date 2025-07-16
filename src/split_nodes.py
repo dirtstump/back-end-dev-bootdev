1a3e80268bc1f1bb4de0d94d8ab86f192992419e
dirt <dirtstump@gmail.com>
2025-07-16 11:40:18 -0500

    adding simple test for split_nodes_delimiter function

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -7,10 +7,10 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if i.text_type != TextType.TEXT:
             raise TypeError("TEXT type only, no nested inline text types")
         new_text = i.text.split(delimiter)
-        for j in new_text:
+        for j, j_text in enumerate(new_text):
             if len(j) < 1:
                 continue
             if j % 2 == 0:
-                new_nodes.append(TextNode(j, TextType.TEXT))
-            new_nodes.append(TextNode(j, text_type))
+                new_nodes.append(TextNode(j_text, TextType.TEXT))
+            new_nodes.append(TextNode(j_text, text_type))
     return new_nodes
