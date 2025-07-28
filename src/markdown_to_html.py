7496112204626c5a0efc4f6d27d8279843f5a5fe
dirt <dirtstump@gmail.com>
2025-07-28 12:38:07 -0500

    got the md to blocks working

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,5 +1,5 @@
 def markdown_to_blocks(markdown):
-    blocks = markdown.split("/n/n")
+    blocks = markdown.split("\n\n")
     blocks = [b.strip() for b in blocks]
     blocks = list(filter(None, blocks))
     return blocks
--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -14,7 +14,7 @@ This is the same paragraph on a new line
 - This is a list
 - with items
 """
-        blocks = markdown_to_blocks
+        blocks = markdown_to_blocks(md)
         test = [
                 "This is **bolded** paragraph",
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
