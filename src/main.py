94a727ffc3fd46df7be763a2986608ca0a1cb57e
dirtstump <dirtstump@gmail.com>
2025-07-31 17:45:44 -0500

    added function to main.py that overwrites a destination directory tree with a source directory tree

--- src/main.py
+++ src/main.py
@@ -1,14 +1,48 @@
 """main function of this directory, nothing really going on yet"""
-from textnode import (
-    TextType,
-    TextNode,
-)
+import os
+import shutil
 
 def main():
     """main function"""
-    text_node = TextNode("anchor text", TextType.BOLD, "www.text.com")
-    print(text_node)
+    source = "static/"
+    destination = "public/"
+    copy_to_destination(source, destination)
 
+def copy_to_destination(source, destination):
+    """function for copying directory and contents to new directory"""
+    print(f"Copying directory tree {source} to {destination}")
+    if not os.path.exists(source):
+        raise ValueError(f"source not found: {source}")
+    if not os.path.exists(destination):
+        raise ValueError(f"destination not found: {destination}")
+    print(f"Removing directory tree: {destination}")
+    shutil.rmtree(destination)
+    os.mkdir(destination)
+    print("Starting recursive function")
+    copy_to_destination_helper(source, destination)
+
+def copy_to_destination_helper(source, destination):
+    """function for copying directory and contents to new directory"""
+    print(f"Current source: {source}")
+    print(f"Current destination: {destination}")
+    if not os.path.exists(source):
+        raise ValueError(f"source not found: {source}")
+    if not os.path.exists(destination):
+        raise ValueError(f"destination not found: {destination}")
+    contents = os.listdir(source)
+    for i in contents:
+        i_path = os.path.join(source, i)
+        i_new = os.path.join(destination, i)
+        if os.path.isfile(i_path):
+            print(f"Copying file: {i_path}")
+            shutil.copy(i_path, i_new)
+            continue
+        if os.path.isdir(i_path):
+            print(f"Making directory: {i_new}")
+            os.mkdir(i_new)
+            copy_to_destination_helper(i_path, i_new)
+            continue
+        raise TypeError("object is neither file nor directory, so idk what's happening here")
 
 if __name__ == "__main__":
     main()
